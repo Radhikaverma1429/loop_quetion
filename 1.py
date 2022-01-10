ab70a9138ed19361d08503d007d950b43b5f422a
Radhika_0014 <79450354+Radhikaverma1429@users.noreply.github.com>
2022-01-10 21:21:05 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+'''n = 2
+for i in range(1, 11, 1):
+    product = n*i
+    print(product)'''
+
+'''a = [10, 20]
+b = a
+b += [30, 40]
+print(a)
+print(b)'''
+
+x = 10
+y = 50
+if (x ** 2 > 100 and y < 100):
+    print(x, y)
